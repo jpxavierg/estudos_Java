8d9bc9294fa77ab7d7b747551b291fd25a0db7bb
jpxavierg <jpxavierg@gmial.com>
2020-05-02 11:34:31 -0300

    Projeto Criado

---
+++ src/application/Program.java
@@ -0,0 +1,45 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Entre com um número:");
+		int x = sc.nextInt();
+		sc.nextLine();
+		String dia;
+		switch (x) {
+		case 1:
+			dia = "domingo";
+			break;
+		case 2:
+			dia = "segunda";
+			break;
+		case 3:
+			dia = "terça";
+			break;
+		case 4:
+			dia = "quarta";
+			break;
+		case 5:
+			dia = "quinta";
+			break;
+		case 6:
+			dia = "sexta";
+		case 7:
+			dia = "sábado";
+			break;
+		default:
+			dia = "Valor inválido!";
+			break;
+		}
+
+		System.out.println("Dia da semana: " + dia);
+		sc.close();
+
+	}
+}
